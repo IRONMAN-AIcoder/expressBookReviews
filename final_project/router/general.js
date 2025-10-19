e0bc18a970aa823c80bd75bfaee59fed7410a8dc
IRONMAN-AIcoder <mdnabhus@gmail.com>
2025-10-19 07:25:09 -0400

    added a new file router/general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,12 +24,26 @@ public_users.post("/register", (req,res) => {
 
 });
 
-// Get the book list available in the shop
+//Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    return res.status(200).send(JSON.stringify(books,null,4));
+   return res.status(200).send(JSON.stringify(books,null,4));
 });
 
-// Get book details based on ISBN
+const axios = require('axios');
+
+public_users.get('/books', (req, res) => {
+    axios.get('https://mdnabhus-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/')
+      .then(response => {
+        return res.status(200).send(JSON.stringify(response.data, null, 4));
+      })
+      .catch(error => {
+        return res.status(500).json({ message: "Error fetching books", error: error.message });
+      });
+});
+
+  
+
+//Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn=req.params.isbn;
   let details=books[isbn];
@@ -40,6 +54,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
     return res.status(404).json({message:"No book in this isbn!"});
   }
  });
+
+ public_users.get('/async-isbn/:isbn',async (req,res)=>{
+    const isbn = req.params.isbn;
+  try {
+    
+    const response = await axios.get(`https://mdnabhus-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai//isbn/${isbn}`);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({
+      message: `Error fetching book with isbn ${isbn}`,
+      error: error.message
+    });
+  }
+} );
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -58,7 +86,21 @@ public_users.get('/author/:author',function (req, res) {
         return res.status(200).json(result);
     }
     else{
-        return res.send(404).json({message:"No book in this author's name is found!"});
+        return res.status(404).json({message:"No book in this author's name is found!"});
+    }
+});
+
+public_users.get('/async-author/:author',async (req,res)=>{
+    const author=req.params.author;
+    try{
+    const response= await axios.get(`https://mdnabhus-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/author/${author}`);
+    return res.status(200).json(response.data);
+    }
+    catch(error){
+        return res.status(500).json({
+            message: `Error fetching book with author ${author}`,
+            error: error.message
+          }); 
     }
 });
 
@@ -79,7 +121,22 @@ public_users.get('/title/:title',function (req, res) {
         return res.status(200).json(result);
     }
     else{
-        return res.send(404).json({message:"No book found with this title!" });
+        return res.status(404).json({message:"No book found with this title!" });
+    }
+});
+
+public_users.get("/async-title/:title", async (req,res)=>{
+    const title=req.params.title;
+    try{
+        const response=await axios.get(`https://mdnabhus-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/title/${title}`);
+        return res.status(200).json(response.data);
+    }
+    catch(error)
+    {
+        return res.status(500).json({
+            message: `Error fetching book with title ${title}`,
+            error: error.message
+          }); 
     }
 });
 
